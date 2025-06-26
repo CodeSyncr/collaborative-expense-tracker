a1d56a18b80173d8afd37c63c08fc525822aa999
CodeSyncr <yashkumar12125@gmail.com>
2025-06-26 20:50:48 +0530

    fix: cleanup

--- next.config.ts
+++ next.config.ts
@@ -8,13 +8,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-    manifest: "/manifest.json",
-  },
 };
 
 export default nextConfig;
